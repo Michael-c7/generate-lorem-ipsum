630bf25cee7b1bb6b759f6176ff3f1711cc277fe
Michael Carr <mcarr2468@gmail.com>
2021-09-13 16:46:42 -0500

    V1.1 finsished basic logic & html / css setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import react,{useState, useEffect} from "react"
-
-import React from 'react'
+import { data } from "./data"
 
 const App = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState(data.slice(0, count))
+
+  const handleClick = e => {
+    e.preventDefault()
+    if(count <= 0) {
+      setCount(0)
+    }
+
+    setText(data.slice(0, count));
+
+  }
+  
   return (
-    <div>
-      hello world
-    </div>
+    <section className="container">
+      <h1>Generate Lorem Ipsum</h1>
+      <form className="form" onSubmit={handleClick}>
+        <label htmlFor="number">Paragraphs:</label>
+        <input type="number" id="number" min="0" max="100" autoFocus={true}
+         onChange={(e) => setCount(e.target.value)}/>
+        <button className="btn">Generate</button>
+      </form>
+      <div className="msg"><sup>*</sup>Generates up to 100 paragraphs at once!</div>
+
+      <section className="generated-text-section">
+        {text.map((item, index) => {
+          return (<p key={index}>{item}</p>)
+        })}
+      </section>
+      
+    </section>
   )
 }
 
